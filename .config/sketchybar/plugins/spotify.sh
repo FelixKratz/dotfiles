62e5b66a32029c71c8c0e4d6a3f44352cf7d4b57
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-06-14 19:01:39 +0200

    sketchybar: silence error for futher scripts

--- .config/sketchybar/plugins/spotify.sh
+++ .config/sketchybar/plugins/spotify.sh
@@ -38,9 +38,9 @@ update () {
   PLAYING=1
   if [ "$(echo "$INFO" | jq -r '.["Player State"]')" = "Playing" ]; then
     PLAYING=0
-    TRACK="$(echo "$INFO" | jq -r .Name | cut -c1-20)"
-    ARTIST="$(echo "$INFO" | jq -r .Artist | cut -c1-20)"
-    ALBUM="$(echo "$INFO" | jq -r .Album | cut -c1-20)"
+    TRACK="$(echo "$INFO" | jq -r .Name 2>/dev/null | cut -c1-20)"
+    ARTIST="$(echo "$INFO" | jq -r .Artist 2>/dev/null | cut -c1-20)"
+    ALBUM="$(echo "$INFO" | jq -r .Album 2>/dev/null | cut -c1-20)"
     SHUFFLE=$(osascript -e 'tell application "Spotify" to get shuffling')
     REPEAT=$(osascript -e 'tell application "Spotify" to get repeating')
     COVER=$(osascript -e 'tell application "Spotify" to get artwork url of current track')
