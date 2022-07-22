a3caa890263b5619ba44e64c46c05f71d2bd222d
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-07-22 15:19:27 +0200

    small tweaks

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -5,20 +5,20 @@ SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
 for i in "${!SPACE_ICONS[@]}"
 do
   sid=$(($i+1))
-  sketchybar --add space space.$sid left                                 \
-             --set space.$sid associated_space=$sid                      \
-                              icon=${SPACE_ICONS[i]}                     \
-                              icon.padding_left=22                       \
-                              icon.padding_right=22                      \
-                              icon.highlight_color=0xffE48FA8            \
-                              background.padding_left=-8                 \
-                              background.padding_right=-8                \
-                              background.height=26                       \
-                              background.corner_radius=9                 \
-                              background.color=0xff3C3E4F                \
-                              background.drawing=on                      \
-                              label.drawing=off                          \
-                              click_script="yabai -m space --focus $sid"
+  sketchybar --add space space.$sid left                         \
+             --set space.$sid associated_space=$sid              \
+                              icon=${SPACE_ICONS[i]}             \
+                              icon.padding_left=22               \
+                              icon.padding_right=22              \
+                              icon.highlight_color=$RED          \
+                              background.padding_left=-8         \
+                              background.padding_right=-8        \
+                              background.height=26               \
+                              background.corner_radius=9         \
+                              background.color=0xff3C3E4F        \
+                              background.drawing=on              \
+                              label.drawing=off                  \
+                              click_script="$SPACE_CLICK_SCRIPT"
 done
 
 sketchybar --add item space_separator left                               \
