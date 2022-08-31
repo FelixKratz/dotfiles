8f2f3968812af35b323f92cb967a2c15678c9fb6
Felix Kratz <felix.kratz@tu-dortmund.de>
2022-08-31 13:10:38 +0200

    faster TOPPROC command

--- .config/sketchybar/plugins/cpu.sh
+++ .config/sketchybar/plugins/cpu.sh
@@ -7,8 +7,7 @@ CPU_INFO=$(ps -eo pcpu,user)
 CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
 CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
 
-TOPPROC=$(ps axo "%cpu,ucomm" | sort -nr | tail +1 | head -n1 | awk '{printf "%.0f%% %s\n", $1, $2}' | sed -e 's/com.apple.//g')
-CPUP=$(echo $TOPPROC | sed -nr 's/([^\%]+).*/\1/p')
+TOPPROC=$(ps -Aceo pid,pcpu,comm -r | awk 'NR==2 {printf "%.0f%% %s\n", $2, $3}' | sed -e 's/com.apple.//g')
 
 CPU_PERCENT="$(echo "$CPU_SYS $CPU_USER" | awk '{printf "%.0f\n", ($1 + $2)*100}')"
 
