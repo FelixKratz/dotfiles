8f96b87a01e37b28cb7d605b3826ee04e3d509db
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-10-25 17:38:12 +0200

    update github notifications after clicking on one

--- .config/sketchybar/items/github.sh
+++ .config/sketchybar/items/github.sh
@@ -24,11 +24,13 @@ github_template=(
   icon.background.y_offset=-12
 )
 
-sketchybar --add item github.bell right                 \
+sketchybar --add event github.update                    \
+           --add item github.bell right                 \
            --set github.bell "${github_bell[@]}"        \
            --subscribe github.bell  mouse.entered       \
                                     mouse.exited        \
                                     mouse.exited.global \
+                                    github.update       \
                                                         \
            --add item github.template popup.github.bell \
            --set github.template "${github_template[@]}"
--- .config/sketchybar/plugins/github.sh
+++ .config/sketchybar/plugins/github.sh
@@ -60,7 +60,7 @@ update() {
       position=popup.github.bell
       icon.background.color=$COLOR
       drawing=on
-      click_script="open \"$URL\"; sketchybar --set github.bell popup.drawing=off"
+      click_script="open \"$URL\"; sketchybar --set github.bell popup.drawing=off; sleep 5; sketchybar --trigger github.update"
     )
 
     args+=(--clone github.notification.$COUNTER github.template \
@@ -79,7 +79,7 @@ popup() {
 }
 
 case "$SENDER" in
-  "routine"|"forced") update
+  "routine"|"forced"|"github.update") update
   ;;
   "mouse.entered") popup on
   ;;
