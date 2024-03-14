cfb749fb7ed2e47b6bb56119bdaa9524cc303980
Patrick-orz <patrick_tang@outlook.com>
2024-03-14 10:12:10 +0800

    fix(Front app icon): Resize app icon to reasonable

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set $NAME label="$INFO" icon.background.image="app.$INFO"
+	sketchybar --set $NAME label="$INFO" icon.background.image="app.$INFO" icon.background.image.scale=0.5
 fi
