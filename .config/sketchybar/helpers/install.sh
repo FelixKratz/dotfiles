cc9f0581f2fa17da825b06dd82f2ed5535363454
Zhizhen He <hezhizhen.yi@gmail.com>
2024-07-25 22:44:09 +0800

    upgrade sketchybar-app-font

--- .config/sketchybar/helpers/install.sh
+++ .config/sketchybar/helpers/install.sh
@@ -11,7 +11,7 @@ brew install --cask sf-symbols
 brew install --cask homebrew/cask-fonts/font-sf-mono
 brew install --cask homebrew/cask-fonts/font-sf-pro
 
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.20/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
 # SbarLua
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
