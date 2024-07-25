c1d9233c5d75384d3f0c3fc9c35a4e8384299c16
Zhizhen He <hezhizhen.yi@gmail.com>
2024-07-25 22:36:50 +0800

    update font installation

--- .config/sketchybar/helpers/install.sh
+++ .config/sketchybar/helpers/install.sh
@@ -8,8 +8,8 @@ brew install sketchybar
 
 # Fonts
 brew install --cask sf-symbols
-brew install --cask homebrew/cask-fonts/font-sf-mono
-brew install --cask homebrew/cask-fonts/font-sf-pro
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
 
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
